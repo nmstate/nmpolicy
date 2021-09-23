72a18b3cc84ac2f39362798fbaf500ad14c223ce
Quique Llorente <ellorent@redhat.com>
2021-09-23 10:42:03 +0200

    ast: Implement eqfilter and path
    The communcation between the parser and the resolver is going to be done
    using a Abstract Syntax Tree or AST for short, also the idea is to start
    representing an expression a filter expression like
    `routes.running.destination=="0.0.0.0/0"`.
    
    This change add the needed types to create a tree to represent that with
    the less priority operator at the top, it contains some unit test to
    check the yaml annotations.
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/nmstate/nmpolicy
 
 go 1.16
 
-require github.com/stretchr/testify v1.7.0
+require (
+	github.com/stretchr/testify v1.7.0
+	sigs.k8s.io/yaml v1.3.0
+)
---
+++ nmpolicy/internal/ast/types_test.go
@@ -0,0 +1,73 @@
+/*
+ * This file is part of the nmpolicy project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2021 Red Hat, Inc.
+ *
+ */
+package ast_test
+
+import (
+	"testing"
+
+	assert "github.com/stretchr/testify/require"
+	"sigs.k8s.io/yaml"
+
+	"github.com/nmstate/nmpolicy/nmpolicy/internal/ast"
+)
+
+func strPtr(str string) *string {
+	return &str
+}
+
+func TestYAML(t *testing.T) {
+
+	astYAML := `
+pos: 1
+eqfilter: 
+- pos: 2 
+  path: 
+  - pos: 3
+    identity: currentState
+- pos: 4
+  path:
+  - pos: 5
+    identity: routes
+  - pos: 6 
+    identity: running
+  - pos: 7 
+    identity: destination
+- pos: 8
+  string: 0.0.0.0/0
+`
+
+	obtainedAST := &ast.Node{}
+	assert.NoError(t, yaml.Unmarshal([]byte(astYAML), obtainedAST))
+
+	expectedAST := &ast.Node{
+		Meta: ast.Meta{Position: 1},
+		EqFilter: &ast.TernaryOperator{
+			{Meta: ast.Meta{Position: 2}, Path: &ast.VariadicOperator{
+				{Meta: ast.Meta{Position: 3}, Terminal: ast.Terminal{Identity: strPtr("currentState")}},
+			}},
+			{Meta: ast.Meta{Position: 4}, Path: &ast.VariadicOperator{
+				{Meta: ast.Meta{Position: 5}, Terminal: ast.Terminal{Identity: strPtr("routes")}},
+				{Meta: ast.Meta{Position: 6}, Terminal: ast.Terminal{Identity: strPtr("running")}},
+				{Meta: ast.Meta{Position: 7}, Terminal: ast.Terminal{Identity: strPtr("destination")}},
+			}},
+			{Meta: ast.Meta{Position: 8}, Terminal: ast.Terminal{String: strPtr("0.0.0.0/0")}},
+		},
+	}
+	assert.Equal(t, expectedAST, obtainedAST)
+}
