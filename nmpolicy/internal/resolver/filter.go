deb5403a58f036d0388999d0b19a863bfe73327d
Alona Kaplan <alkaplan@redhat.com>
2021-10-19 15:25:46 +0300

    resolver: implement equilaty filter operation
    Traverse a yaml and return a snippet contaiting only the high level
    entities answering the filtering condition.
    
    Signed-off-by: Alona Kaplan <alkaplan@redhat.com>

---
+++ nmpolicy/internal/resolver/filter.go
@@ -0,0 +1,53 @@
+package resolver
+
+import (
+	"fmt"
+
+	"github.com/nmstate/nmpolicy/nmpolicy/internal/ast"
+)
+
+func filter(inputState map[string]interface{}, path ast.VariadicOperator, expectedNode ast.Node) (map[string]interface{}, error) {
+	filtered, err := applyFuncOnPath(inputState, path, expectedNode, mapContainsValue, true)
+
+	if err != nil {
+		return nil, filterError(fmt.Errorf("error applying operation on the path : %v", err))
+	}
+
+	if filtered == nil {
+		return nil, nil
+	}
+
+	filteredMap, ok := filtered.(map[string]interface{})
+	if !ok {
+		return nil, filterError(fmt.Errorf("error converting filtering result to a map"))
+	}
+	return filteredMap, nil
+}
+
+func isEqual(obtainedValue interface{}, desiredValue ast.Node) (bool, error) {
+	if desiredValue.String != nil {
+		stringToCompare, ok := obtainedValue.(string)
+		if !ok {
+			return false, fmt.Errorf("the value %v of type %T not supported,"+
+				"curretly only string values are supported", obtainedValue, obtainedValue)
+		}
+		return stringToCompare == *desiredValue.String, nil
+	}
+
+	return false, fmt.Errorf("the desired value %v is not supported. Curretly only string values are supported", desiredValue)
+}
+
+func mapContainsValue(mapToFilter map[string]interface{}, filterKey string, expectedNode ast.Node) (interface{}, error) {
+	obtainedValue, ok := mapToFilter[filterKey]
+	if !ok {
+		return nil, filterError(fmt.Errorf("cannot find key %s in %v", filterKey, obtainedValue))
+	}
+	valueIsEqual, err := isEqual(obtainedValue, expectedNode)
+	if err != nil {
+		return nil, filterError(fmt.Errorf("error comparing the expected and obtained values : %v", err))
+	}
+	if valueIsEqual {
+		return mapToFilter, nil
+	}
+	return nil, nil
+}
