ee903b9590b09de630269c28f0ece6933f8ed3ba
Quique Llorente <ellorent@redhat.com>
2021-12-01 14:44:02 +0100

    cli: Implement main using pflag
    The nmpolicy should implement at cli that is able to generate a
    desiredState from a NMPolicy. This change implement just that using the
    library https://github.com/spf13/pflag to parse the arguments.
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- cmd/nmpolicy/main.go
+++ cmd/nmpolicy/main.go
@@ -17,16 +17,89 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
+	flag "github.com/spf13/pflag"
+	"sigs.k8s.io/yaml"
+
 	"github.com/nmstate/nmpolicy/nmpolicy"
 	"github.com/nmstate/nmpolicy/nmpolicy/types"
 )
 
+var (
+	policyPath               string
+	currentStatePath         string
+	capturedStatesInputPath  string
+	capturedStatesOutputPath string
+	desiredStatePath         string
+	help                     bool
+)
+
 func main() {
-	policySpec := types.PolicySpec{}
-	cachedState := types.CachedState{}
-	currentState := []byte{}
-	_, err := nmpolicy.GenerateState(policySpec, currentState, cachedState)
-	if err != nil {
-		panic(err)
+	flag.StringVar(&policyPath, "policy", "policy.yaml", "input file path to NNPolicy")
+	flag.StringVar(&currentStatePath, "current-state", "current-state.yaml", "input file path to current NMState")
+	flag.StringVar(&capturedStatesInputPath, "captured-states-input", "captured-states-input.yaml",
+		"input file path to the captured NMStates from previous run")
+	flag.StringVar(&desiredStatePath, "desired-state", "desired-state.yaml", "output file for the generated NMState")
+	flag.StringVar(&capturedStatesOutputPath, "captured-states-output", "captured-states-output.yaml",
+		"output file path to the captured NMStates from previous run")
+	flag.BoolVarP(&help, "help", "h", false, "")
+
+	flag.Parse()
+
+	if help {
+		flag.Usage()
 	}
+
+	policy := types.PolicySpec{}
+	if err := unmarshalFromPath(policyPath, &policy); err != nil {
+		log.Fatalf("failed reading policy: %v", err)
+	}
+	capturedStatesInput := types.CachedState{}
+	if err := unmarshalFromPath(capturedStatesInputPath, &capturedStatesInput); err != nil {
+		log.Fatalf("failed reading captured states: %v", err)
+	}
+
+	currentState, err := os.ReadFile(currentStatePath)
+	if err != nil {
+		log.Fatalf("failed reading current state: %v", err)
+	}
+
+	generatedState, err := nmpolicy.GenerateState(policy, currentState, capturedStatesInput)
+	if err != nil {
+		log.Fatalf("failed generating desired state: %v", err)
+	}
+
+	if err := marshalToPath(desiredStatePath, generatedState.DesiredState); err != nil {
+		log.Fatalf("failed writing desired state: %v", err)
+	}
+
+	if err := marshalToPath(capturedStatesOutputPath, generatedState.Cache); err != nil {
+		log.Fatalf("failed writing captured states: %v", err)
+	}
+}
+
+func unmarshalFromPath(path string, unmarshaled interface{}) error {
+	marshaled, err := os.ReadFile(path)
+	if err != nil {
+		return fmt.Errorf("failed reading file to unmarshal it: %w", err)
+	}
+
+	if err := yaml.Unmarshal(marshaled, unmarshaled); err != nil {
+		return fmt.Errorf("failed unmarshaling %s: %w", path, err)
+	}
+	return nil
+}
+
+func marshalToPath(path string, unmarshaled interface{}) error {
+	marshaled, err := yaml.Marshal(unmarshaled)
+	if err != nil {
+		return fmt.Errorf("failed marshaling %s: %w", path, err)
+	}
+	if err := os.WriteFile(path, marshaled, 0); err != nil {
+		return fmt.Errorf("failed writing file with marshaled data: %w", err)
+	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/nmstate/nmpolicy
 go 1.16
 
 require (
+	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.7.0
 	sigs.k8s.io/yaml v1.3.0
 )
