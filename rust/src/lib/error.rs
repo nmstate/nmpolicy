edd7ca593dea46a44b4719e19233a4fc7c7f39af
Enrique Llorente <ellorent@redhat.com>
2022-07-11 12:28:20 +0200

    rust, errors: Reduce the number of variants
    The tokens, parser error variants are not going to be matched by users,
    let's just pass a message and also add a simple simple mechanism to add
    context to errors using a builder pattern.
    
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- rust/src/lib/error.rs
+++ rust/src/lib/error.rs
@@ -5,39 +5,8 @@ use crate::snippet::snippet;
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[non_exhaustive]
 pub enum ErrorKind {
-    // Tokenizer
-    IllegalChar(char),
-    InvalidEqFilterMissingEqual(char),
-    InvalidEqFilterEOF,
-    InvalidReplaceMissingEqual(char),
-    InvalidReplaceEOF,
-    InvalidStringMissingDelimiter(char),
-    InvalidNumberFormat(char),
-    InvalidIdentityFormat(char),
-
-    // Parser
-    InvalidExpressionUnexpectedToken(String),
-    InvalidPipeMissingLeftPath,
-    InvalidPipeMissingLeftExpression,
-    InvalidPipeMissingRightExpression,
-    InvalidPathUnexpectedTokenAfterDot,
-    InvalidPathMissingDot,
-    InvalidTernaryUnexpectedRightHand(&'static str),
-    InvalidTernaryMissingRightHand(&'static str),
-    InvalidTernaryUnexpectedLeftHand(&'static str),
-    InvalidTernaryMissingLeftHand(&'static str),
-
-    // Resolver
-    ResolveErrorCaptureEntryNotFound(String),
-    ResolveErrorNotSupportedValue,
-    ResolveErrorInvalidPathInputSource(String),
-    ResolveErrorInvalidInputSource(String),
-    ResolveErrorUnsupportedOperation(String),
-    ResolveErrorFailingFilteringPath(String),
-    ResolveErrorFailedFilterResultConvertion(serde_json::Value),
-    PathErrorUnexpectedMapStep(serde_json::Map<String, serde_json::Value>),
-    PathErrorStepInvalidType(serde_json::Value, String),
-
+    ValidationError,
+    EvaluationError,
     Bug,
     NotImplementedError,
     NotSupportedError,
@@ -45,119 +14,11 @@ pub enum ErrorKind {
 
 impl std::fmt::Display for NmpolicyError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if self.expression.is_empty() {
-            write!(f, "{}", self.kind)
-        } else {
-            write!(
-                f,
-                "{}\n{}",
-                self.kind,
-                snippet(self.expression.clone(), self.pos)
-            )
-        }
-    }
-}
-
-impl std::fmt::Display for ErrorKind {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            ErrorKind::IllegalChar(ch) => write!(f, "illegal char {ch}"),
-            ErrorKind::InvalidEqFilterMissingEqual(ch) => write!(
-                f,
-                "invalid EQFILTER operation format ({ch} is not equal char)"
-            ),
-            ErrorKind::InvalidEqFilterEOF => write!(f, "invalid EQFILTER operation format (EOF)"),
-            ErrorKind::InvalidReplaceMissingEqual(ch) => write!(
-                f,
-                "invalid REPLACE operation format ({ch} is not equal char)"
-            ),
-            ErrorKind::InvalidReplaceEOF => write!(f, "invalid REPLACE operation format (EOF)"),
-            ErrorKind::InvalidStringMissingDelimiter(delimiter) => {
-                write!(f, "invalid string format (missing {delimiter} terminator)")
+        match self.expression.clone() {
+            Some(expression) => {
+                write!(f, "{}\n{}", self.msg, snippet(expression, self.pos))
             }
-            ErrorKind::InvalidNumberFormat(ch) => {
-                write!(f, "invalid number format ({ch} is not a digit)")
-            }
-            ErrorKind::InvalidIdentityFormat(ch) => write!(
-                f,
-                "invalid identity format ({ch} is not a digit, letter or -)"
-            ),
-            ErrorKind::InvalidExpressionUnexpectedToken(literal) => {
-                write!(f, "invalid expression: unexpected token `{}`", literal)
-            }
-            ErrorKind::InvalidPipeMissingLeftPath => {
-                write!(f, "invalid pipe: only paths can be piped in",)
-            }
-            ErrorKind::InvalidPipeMissingLeftExpression => {
-                write!(f, "invalid pipe: missing pipe in expression",)
-            }
-            ErrorKind::InvalidPipeMissingRightExpression => {
-                write!(f, "invalid pipe: missing pipe out expression",)
-            }
-            ErrorKind::InvalidPathUnexpectedTokenAfterDot => {
-                write!(f, "invalid path: missing identity or number after dot",)
-            }
-            ErrorKind::InvalidPathMissingDot => {
-                write!(f, "invalid path: missing dot",)
-            }
-            ErrorKind::InvalidTernaryUnexpectedRightHand(operator_name) => {
-                write!(
-                    f,
-                    "invalid {operator_name}: right hand argument is not a string or identity"
-                )
-            }
-            ErrorKind::InvalidTernaryUnexpectedLeftHand(operator_name) => {
-                write!(
-                    f,
-                    "invalid {operator_name}: left hand argument is not a path"
-                )
-            }
-            ErrorKind::InvalidTernaryMissingRightHand(operator_name) => {
-                write!(f, "invalid {operator_name}: missing right hand argument")
-            }
-            ErrorKind::InvalidTernaryMissingLeftHand(operator_name) => {
-                write!(f, "invalid {operator_name}: missing left hand argument")
-            }
-            ErrorKind::ResolveErrorCaptureEntryNotFound(capture_entry_name) => {
-                write!(f, "capture entry '{capture_entry_name}' not found")
-            }
-            ErrorKind::ResolveErrorNotSupportedValue => {
-                write!(
-                    f,
-                    "not supported value. Only string or capture entry path are supported"
-                )
-            }
-            ErrorKind::ResolveErrorInvalidPathInputSource(node) => {
-                write!(
-                    f,
-                    "invalid path input source ({node}), only capture reference is supported"
-                )
-            }
-            ErrorKind::ResolveErrorInvalidInputSource(node) => {
-                write!(f, "invalid input source ({node}), only current state or capture reference is supported")
-            }
-            ErrorKind::ResolveErrorUnsupportedOperation(node) => {
-                write!(f, "root node has unsupported operation : {node}")
-            }
-            ErrorKind::ResolveErrorFailingFilteringPath(error) => {
-                write!(f, "failed applying operation on the path: {error}")
-            }
-            ErrorKind::ResolveErrorFailedFilterResultConvertion(value) => {
-                write!(f, "failed converting filtering result `{value}` to a map")
-            }
-            ErrorKind::PathErrorUnexpectedMapStep(map) => {
-                write!(
-                    f,
-                    "path error: unexpected non identity step for map state '{map:?}'"
-                )
-            }
-            ErrorKind::PathErrorStepInvalidType(input_state, step) => {
-                write!(f, "invalid type {input_state:?} for identity step '{step}'")
-            }
-
-            ErrorKind::Bug => write!(f, "bug"),
-            ErrorKind::NotImplementedError => write!(f, "not implemented"),
-            ErrorKind::NotSupportedError => write!(f, "not supported"),
+            None => write!(f, "{}", self.msg),
         }
     }
 }
@@ -165,19 +26,20 @@ impl std::fmt::Display for ErrorKind {
 impl Error for NmpolicyError {}
 
 #[derive(Clone, Debug, Eq, PartialEq)]
-#[non_exhaustive]
 pub struct NmpolicyError {
     kind: ErrorKind,
-    expression: String,
+    expression: Option<String>,
     pos: usize,
+    msg: String,
 }
 
 impl NmpolicyError {
     pub fn new(kind: ErrorKind) -> Self {
         Self {
             kind,
-            expression: "".to_string(),
+            expression: None,
             pos: 0,
+            msg: String::from(""),
         }
     }
 
@@ -185,8 +47,65 @@ impl NmpolicyError {
         self.kind.clone()
     }
 
-    pub fn decorate(&mut self, expression: String, pos: usize) {
-        self.expression = expression;
-        self.pos = pos;
+    pub fn decorate(self, expression: String, pos: usize) -> Self {
+        self.expression(Some(expression)).pos(pos)
     }
+
+    pub fn ctx(mut self, ctx: String) -> Self {
+        if !self.msg.contains(ctx.as_str()) {
+            self.msg = format!("{}: {}", ctx, self.msg);
+        }
+        self
+    }
+
+    pub fn pos(mut self, pos: usize) -> Self {
+        if self.pos == 0 {
+            self.pos = pos;
+        }
+        self
+    }
+
+    pub fn expression(mut self, expression: Option<String>) -> Self {
+        self.expression = expression;
+        self
+    }
+
+    pub fn resolver(self) -> Self {
+        self.ctx("resolve error".to_string())
+    }
+    pub fn eqfilter(self) -> Self {
+        self.ctx("eqfilter error".to_string())
+    }
+    pub fn path(self, pos: usize) -> Self {
+        self.pos(pos).ctx("invalid path".to_string())
+    }
+}
+
+impl From<String> for NmpolicyError {
+    fn from(error: String) -> NmpolicyError {
+        evaluation_error(error)
+    }
+}
+
+impl From<serde_json::Error> for NmpolicyError {
+    fn from(error: serde_json::Error) -> NmpolicyError {
+        evaluation_error(error.to_string())
+    }
+}
+
+pub(crate) fn error(kind: ErrorKind, msg: String) -> NmpolicyError {
+    NmpolicyError {
+        kind,
+        msg,
+        expression: None,
+        pos: 0,
+    }
+}
+
+pub(crate) fn validation_error(msg: String) -> NmpolicyError {
+    error(ErrorKind::ValidationError, msg)
+}
+
+pub(crate) fn evaluation_error(msg: String) -> NmpolicyError {
+    error(ErrorKind::EvaluationError, msg)
 }
