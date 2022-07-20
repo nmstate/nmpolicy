097d2c791bd4f987fbdea985a013a88e14905b4e
Enrique Llorente <ellorent@redhat.com>
2022-07-20 16:53:02 +0200

    rust: Glue code for lex/parse/resolve
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- rust/src/lib/types.rs
+++ rust/src/lib/types.rs
@@ -2,19 +2,9 @@ use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-use crate::ast::node::Node;
-
 pub(crate) type NMState = serde_json::Map<String, serde_json::Value>;
 
-pub(crate) type Capture = HashMap<String, CaptureEntry>;
-
 pub(crate) type CapturedStates = HashMap<String, CapturedState>;
-
-#[derive(Clone, Eq, PartialEq, Debug, Serialize, Deserialize)]
-pub(crate) struct CaptureEntry {
-    pub expression: String,
-    pub ast: Box<Node>,
-}
 
 #[derive(Clone, Eq, PartialEq, Debug, Serialize, Deserialize)]
 pub(crate) struct MetaInfo {
